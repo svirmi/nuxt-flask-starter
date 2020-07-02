35ac3675c47abace848bfab4fe619e093fc1a1cd
Mikhail Svirkin <mikhail.svirkin@admiralmarkets.com>
2020-07-02 07:51:10 +0300

    improvements

--- api/manage.py
+++ api/manage.py
@@ -11,7 +11,7 @@ class Product(Resource):
             'product' : [
                 "Product 1",
                 "Product 2",
-                "Product 3"
+                "Product 333"
             ]
         }
 api.add_resource(Product, '/api')
